0fab1cc89b2497d8170b778c93954f0e4886fe3d
MaverickEdwards <r.edwards0917@gmail.com>
2023-01-02 12:48:58 -0500

    commit tree 9d8ec7d43e9d644b89f2ace0fbdc78d071343629

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function createEmployeeRecord(firstName, familyName, title, payPerHour){
+    return {
+        firstName: 'Gray',
+        familyName: 'Worm',
+        title: 'Security',
+        payPerHour: 1,
+        timeInEvents: [],
+        timeOutEvents: [],
+
+    }
+}
+
+function createEmployeeRecords(){
+    for (let i = 0; i < createEmployeeRecord / 2; i++){
+        
+    }
+}
+
+function createTimeInEvent(object){
+    return dateStamp
+}
+
+let dateStamp = ("YYYY-MM-DD HHMM")
+
+function createTimeOutEvent(){
+
+}
